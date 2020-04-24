f24c71c66f495506b7ac9b6601e0377def7671a5
Patrick Kille <pr.kille@gmail.com>
2020-04-24 09:24:50 -0700

    create basic header and app components

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <div>
+      <h1>Welcome to the Epicodus Tap Room!</h1>
+      <p>Formerly known as The Rialto</p>
+    </div>
+  );
+}
+
+export default Header;
