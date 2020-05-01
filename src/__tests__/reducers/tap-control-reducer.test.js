ba6b62ae5b4c9629ac35d0b09797d9c388fe59dd
Patrick Kille <pr.kille@gmail.com>
2020-05-01 09:08:06 -0700

    test delete function

--- src/__tests__/reducers/tap-control-reducer.test.js
+++ src/__tests__/reducers/tap-control-reducer.test.js
@@ -10,10 +10,29 @@ let tapList = {
   id: 1
 };
 
+let emptyTaps = {
+  1: {
+    name: "Ranier",
+    brand: "Ranier",
+    price: "$3",
+    abv: "4.6%",
+    inventory: 0,
+    id: 1
+  },
+  2: {
+    name: "Old Rasputin Russian Imperial Stout",
+    brand: "North Coast Brewing Co",
+    price: "$6",
+    abv: "9.0%",
+    inventory: 24,
+    id: 2
+  }
+}
+
 describe('tapControlReducer', () => {
   test('Should return default state if no action type is passed to reducer', () => {
     expect(tapControlReducer({}, {type:null})).toEqual({});
-  })
+  });
 
   test('Should add new keg to tapList', () => {
     const {name,brand,price,abv,inventory,id} = tapList;
@@ -35,6 +54,24 @@ describe('tapControlReducer', () => {
         abv: abv,
         inventory: inventory,
         id: id
+      }
+    });
+  });
+
+  test('Should delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+
+    expect(tapControlReducer(emptyTaps, action)).toEqual({
+      2: {
+        name: "Old Rasputin Russian Imperial Stout",
+        brand: "North Coast Brewing Co",
+        price: "$6",
+        abv: "9.0%",
+        inventory: 24,
+        id: 2
       }
     });
   });
